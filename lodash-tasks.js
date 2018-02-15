bef82e15776601f07b3afe45820d92073722d37b
hollymrogers <36003334+hollymrogers@users.noreply.github.com>
2018-02-15 16:16:14 +0000

    Filter members of family under 50

--- lodash-tasks.js
+++ lodash-tasks.js
@@ -7,3 +7,6 @@ _.sumBy(gillFamily, 'age')
 var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}]
 _.meanBy(gillFamily, 'age')
 
+var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}]
+_.filter(gillFamily, function(o) { return o.age <50;})
+
