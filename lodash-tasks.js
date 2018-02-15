dea77a3010285488f5ab1fa12722bc54f8840b86
hollymrogers <36003334+hollymrogers@users.noreply.github.com>
2018-02-15 16:33:55 +0000

    Calculate amount under 50

--- lodash-tasks.js
+++ lodash-tasks.js
@@ -12,3 +12,7 @@ _.filter(gillFamily, function(o) { return o.age <50;})
 
 var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}]
 _.size(gillFamily);
+
+var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}]
+_.remove(gillFamily, function(o) { return o.age >50;})
+_.size(gillFamily);
