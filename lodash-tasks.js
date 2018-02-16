9728c2924392ccf65cbc411369e4acdd564487fd
hollymrogers <36003334+hollymrogers@users.noreply.github.com>
2018-02-16 11:11:29 +0000

    include question numbers

--- lodash-tasks.js
+++ lodash-tasks.js
@@ -1,18 +1,20 @@
+//1
 var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}]
 _.map(gillFamily, 'name')
 
-var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}]
+//3
 _.sumBy(gillFamily, 'age')
 
-var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}]
+//4
+'robin', age: 60}]
 _.meanBy(gillFamily, 'age')
 
-var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}]
+//5
 _.filter(gillFamily, function(o) { return o.age <50;})
 
-var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}]
+//6
 _.size(gillFamily);
 
-var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}]
+//7
 _.remove(gillFamily, function(o) { return o.age >50;})
 _.size(gillFamily);
