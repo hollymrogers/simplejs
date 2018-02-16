756fdf3b6584d8c3acc052f4f6982ea4cd4ac64e
hollymrogers <36003334+hollymrogers@users.noreply.github.com>
2018-02-16 13:54:07 +0000

    add variables

--- lodash-tasks.js
+++ lodash-tasks.js
@@ -1,19 +1,30 @@
 //1
 var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}]
-_.map(gillFamily, 'name')
+var gillName
+gillName = _.map(gillFamily, 'name')
 
 //3
-_.sumBy(gillFamily, 'age')
+var sumOfGillAges
+sumofGillAges = _.sumBy(gillFamily, 'age')
 
 //4
-_.meanBy(gillFamily, 'age')
+var meanOfGillAges
+meanOfGillAges = _.meanBy(gillFamily, 'age')
 
 //5
-_.filter(gillFamily, function(o) { return o.age <50;})
+var gillUnderFifty
+gillUnder50 = _.filter(gillFamily, function(o) { return o.age <50;})
 
 //6
-_.size(gillFamily);
+var gillFamilySize
+gillFamilySize = _.size(gillFamily);
 
 //7
+var gillUnderFiftySize
 _.remove(gillFamily, function(o) { return o.age >50;})
-_.size(gillFamily);
+gillUnderFiftySize = _.size(gillFamily);
+
+//11
+var sortGillAge
+sortGillAge = _.sortBy(gillFamily, ['age'])
+
