af61a89d179eb9b7363dab23e9e3bd464233b572
hollymrogers <36003334+hollymrogers@users.noreply.github.com>
2018-02-19 10:19:33 +0000

    produce array including surnames

--- lodash-tasks.js
+++ lodash-tasks.js
@@ -3,6 +3,15 @@ var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'd
 var gillName
 gillName = _.map(gillFamily, 'name')
 
+//2
+var gillName
+gillName = _.map(gillFamily, 'name')
+var greet = function(name) {
+    return name + " gill";
+},
+    greetings = _.map(gillName, greet);
+console.log(greetings)
+
 //3
 var sumOfGillAges
 sumofGillAges = _.sumBy(gillFamily, 'age')
