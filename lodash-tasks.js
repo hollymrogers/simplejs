f245902c3cad4397638ac1dc2d574109aa16a3c2
hollymrogers <36003334+hollymrogers@users.noreply.github.com>
2018-02-21 12:50:24 +0000

    Shorten code

--- lodash-tasks.js
+++ lodash-tasks.js
@@ -1,11 +1,9 @@
 //1
 var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}]
-var gillName
-gillName = _.map(gillFamily, 'name')
+var gillName = _.map(gillFamily, 'name')
 
 //2
-var gillName
-gillName = _.map(gillFamily, 'name')
+var gillName = _.map(gillFamily, 'name')
 var greet = function(name) {
     return name + " gill";
 },
@@ -13,20 +11,17 @@ var greet = function(name) {
 console.log(greetings)
 
 //3
-var sumOfGillAges
-sumofGillAges = _.sumBy(gillFamily, 'age')
+var sumOfGillAges = _.sumBy(gillFamily, 'age')
 
 //4
-var meanOfGillAges
-meanOfGillAges = _.meanBy(gillFamily, 'age')
+var meanOfGillAges = _.meanBy(gillFamily, 'age')
 
 //5
 var gillUnderFifty
 gillUnder50 = _.filter(gillFamily, function(o) { return o.age <50;})
 
 //6
-var gillFamilySize
-gillFamilySize = _.size(gillFamily);
+var gillFamilySize = _.size(gillFamily);
 
 //7
 var gillUnderFifty = _.filter(gillFamily, function(o) { return o.age <50;})
@@ -46,12 +41,10 @@ var greet = function(name) {
 _.join(greetings,", ")
 
 //11
-var sortGillAge
-sortGillAge = _.sortBy(gillFamily, ['age'])
+var sortGillAge = _.sortBy(gillFamily, ['age'])
 
 //14
-var youngestGill
-youngestGill = _.minBy(gillFamily, 'age')
+var youngestGill = _.minBy(gillFamily, 'age')
 
 //16
 var gillName = _.map(gillFamily, 'name')
