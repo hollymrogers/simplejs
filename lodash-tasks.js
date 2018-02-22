4498278cf3ed61a9af076ebc9dd86ba28750353c
hollymrogers <36003334+hollymrogers@users.noreply.github.com>
2018-02-22 23:01:00 +0000

    update tasks

--- lodash-tasks.js
+++ lodash-tasks.js
@@ -3,12 +3,10 @@ var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'd
 var gillName = _.map(gillFamily, 'name')
 
 //2
-var gillName = _.map(gillFamily, 'name')
-var greet = function(name) {
-    return name + " gill";
-},
-    greetings = _.map(gillName, greet);
-console.log(greetings)
+var gillFamilySurname = _.map(gillFamily, function(familyMember) {
+ return familyMember.name + " gill"
+})
+console.log(gillFamilySurname)
 
 //3
 var sumOfGillAges = _.sumBy(gillFamily, 'age')
@@ -40,8 +38,23 @@ var greet = function(name) {
     greetings = _.map(gillName, greet);
 _.join(greetings,", ")
 
+//9
+var gillFamilyTable = "<tr><th>Name</th> <th>Age</th></tr>" 
+_.forEach(gillFamily, function(familyMember){
+ var familyMemberRow = "<tr><td>" + familyMember.name + "</td><td>" + familyMember.age+ "</td></tr>"
+gillFamilyTable += familyMemberRow
+})
+var wrappedTable = "<table>" + gillFamilyTable + "</table>"
+console.log(wrappedTable)
+
 //11
 var sortGillAge = _.sortBy(gillFamily, ['age'])
+
+//12
+var gillFamilyLetterD = _.filter(gillFamily, function(familyMember) {
+var firstLetter = familyMember.name.charAt(0) 
+ return firstLetter === 'd' || firstLetter === 'D'
+})
 
 //14
 var youngestGill = _.minBy(gillFamily, 'age')
@@ -52,7 +65,8 @@ var results = _.filter(gillFamily, function(item) { return item.name.indexOf(let
 console.log(results)
 
 //16
-var gillName = _.map(gillFamily, 'name')
-var gillUpperCase = _.startCase(_.toLower(gillName));
-var gillUpperString = _.words(gillUpperCase) 
-console.log(gillUpperCase)
+var gillFamilyCapitalised = _.map(gillFamily, function(familyMember) { 
+	var capitaliseName = _.capitalize(familyMember.name)
+	return {...familyMember, name: capitaliseName}
+})
+console.log(gillFamilyCapitalised)
